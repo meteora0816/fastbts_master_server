5e9008f02f12b8a2e1562428291083d9f8d230fe
meteora <lixingyao816@gmail.com>
2022-02-07 14:52:08 +0800

    release

--- Server_Selector.go
+++ Server_Selector.go
@@ -11,41 +11,28 @@ import (
 )
 import "github.com/gin-gonic/gin"
 
-var servers = []string{"101.35.92.214", "101.35.86.228", "101.35.9.228", "101.35.9.114", "110.42.169.86", "121.5.26.137", "1.116.117.183", "1.15.30.244", // Shanghai
-					"49.232.210.247", "152.136.120.165", "152.136.124.173", "49.232.128.240", "81.70.193.140", "49.232.129.114", "62.234.117.45", "81.70.55.189"} // Beijing
-const maxBandwidth float64 = 200 // bandwidth limit for each server
-
-type SafeMap struct {
-	bandwidthUsed map[string]float64
-	mux           sync.RWMutex
-}
-
 var r *gin.Engine
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
 type Config struct {
-	CISSleep            int     `yaml:"cis_sleep"`
-	DownloadSizeSleep   int     `yaml:"download_size_sleep"`
-	TimeWindow          int     `yaml:"time_window"`
-	TestTimeout         int     `yaml:"test_timeout"`
-	GetInfoInterval     int     `yaml:"get_info_interval"`
-	MaxTrafficUse4g     int     `yaml:"max_traffic_use_4_g"`
-	MaxTrafficUse5g     int     `yaml:"max_traffic_use_5_g"`
-	MaxTrafficUseWifi   int     `yaml:"max_traffic_use_wifi"`
-	MaxTrafficUseOthers int     `yaml:"max_traffic_use_others"`
-	KSimilar            int     `yaml:"k_similar"`
-	Threshold           float64 `yaml:"threshold"`
+	BPSleep             int      `yaml:"bp_sleep"`
+	DownloadSizeSleep   int      `yaml:"download_size_sleep"`
+	TimeWindow          int      `yaml:"time_window"`
+	TestTimeout         int      `yaml:"test_timeout"`
+	GetInfoInterval     int      `yaml:"get_info_interval"`
+	MaxTrafficUse4g     int      `yaml:"max_traffic_use_4_g"`
+	MaxTrafficUse5g     int      `yaml:"max_traffic_use_5_g"`
+	MaxTrafficUseWifi   int      `yaml:"max_traffic_use_wifi"`
+	MaxTrafficUseOthers int      `yaml:"max_traffic_use_others"`
+	KSimilar            int      `yaml:"k_similar"`
+	Threshold           float64  `yaml:"threshold"`
+	MaxBandwidth        float64  `yaml:"max_bandwidth"` // bandwidth limit for each server
+	Servers             []string `yaml:"servers"`
 }
 
 var GlobalConfig Config
 
 func init() {
+	GlobalConfig = Config{}
 	config, err := ioutil.ReadFile("./config.yaml")
 	if err != nil {
 		fmt.Print(err)
@@ -54,7 +41,7 @@ func init() {
 	if err != nil {
 		fmt.Print(err)
 	} else {
-		fmt.Println(GlobalConfig)
+		fmt.Printf("config: %+v\n", GlobalConfig)
 	}
 }
 
@@ -66,9 +53,7 @@ func getBandwidthUsed(ip string) float64 {
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	//fmt.Println(string(body))
 	bandwidthUsed, _ := strconv.ParseFloat(string(body), 64)
-	//fmt.Println(bandwidthUsed)
 	return bandwidthUsed * 8 / 1024 / 1024
 }
 
@@ -77,12 +62,12 @@ type bdu struct {
 	bd float64 // bandwidthUsed
 }
 
-func SS(eBandwidth float64, ServersSortedByRTT []string) (int, []string) {
+func SS(eBandwidth float64) (int, []string) {
 	var bandwidthUsed []bdu
 	wg := sync.WaitGroup{}
-	wg.Add(len(servers))
-	buCh := make(chan bdu, len(servers))
-	for _, ip := range servers {
+	wg.Add(len(GlobalConfig.Servers))
+	buCh := make(chan bdu, len(GlobalConfig.Servers))
+	for _, ip := range GlobalConfig.Servers {
 		ip := ip
 		go func() {
 			bandwidthUsed := bdu{ip: ip, bd: getBandwidthUsed(ip)}
@@ -94,9 +79,7 @@ func SS(eBandwidth float64, ServersSortedByRTT []string) (int, []string) {
 	close(buCh)
 	for bu := range buCh {
 		bandwidthUsed = append(bandwidthUsed, bu)
-		//fmt.Println(bu.ip, bu.bd)
 	}
-	//fmt.Println(bandwidthUsed)
 	sort.Slice(bandwidthUsed, func(i, j int) bool {
 		return bandwidthUsed[i].bd < bandwidthUsed[j].bd
 	})
@@ -104,7 +87,7 @@ func SS(eBandwidth float64, ServersSortedByRTT []string) (int, []string) {
 	num := 0
 	var ipList []string
 	for _, bu := range bandwidthUsed {
-		rest := maxBandwidth - bu.bd
+		rest := GlobalConfig.MaxBandwidth - bu.bd
 		if rest <= 0 {
 			continue
 		} else {
@@ -124,10 +107,9 @@ func SS(eBandwidth float64, ServersSortedByRTT []string) (int, []string) {
 }
 
 func main() {
-	fmt.Println("start")
 	r = gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, `hello, FastBTS!`)
+		c.JSON(http.StatusOK, `hello, Swiftest!`)
 	})
 	r.GET("/speedtest/iplist/available", func(c *gin.Context) {
 		type Res struct {
@@ -136,8 +118,8 @@ func main() {
 			ClientIP  string   `json:"client_ip"`
 		}
 		var res Res
-		res.ServerNum = len(servers)
-		res.IpList = servers
+		res.ServerNum = len(GlobalConfig.Servers)
+		res.IpList = GlobalConfig.Servers
 		res.ClientIP = c.ClientIP()
 		c.JSON(http.StatusOK, res)
 	})
@@ -148,7 +130,6 @@ func main() {
 		}
 		var req Req
 		err := c.BindJSON(&req)
-		//fmt.Println(req.NetworkType)
 		if err != nil {
 			fmt.Println("err")
 			return
@@ -158,7 +139,7 @@ func main() {
 			IpList            []string `json:"ip_list"`
 			TestTimeout       int      `json:"test_timeout"`
 			DownloadSizeSleep int      `json:"download_size_sleep"`
-			CISSleep          int      `json:"cis_sleep"`
+			BPSleep           int      `json:"bp_sleep"`
 			TimeWindow        int      `json:"time_window"`
 			KSimilar          int      `json:"k_similar"`
 			MaxTrafficUse     int      `json:"max_traffic_use"`
@@ -166,7 +147,7 @@ func main() {
 			GetInfoInterval   int      `json:"get_info_interval"`
 		}
 		var res Res
-		res.CISSleep = GlobalConfig.CISSleep
+		res.BPSleep = GlobalConfig.BPSleep
 		res.DownloadSizeSleep = GlobalConfig.DownloadSizeSleep
 		res.TimeWindow = GlobalConfig.TimeWindow
 		res.TestTimeout = GlobalConfig.TestTimeout
@@ -188,48 +169,7 @@ func main() {
 			eBandwidth = 500
 			res.MaxTrafficUse = GlobalConfig.MaxTrafficUseOthers
 		}
-		res.ServerNum, res.IpList = SS(eBandwidth, req.ServersSortedByRTT)
-		//num := 4
-		//if req.NetworkType == "LTE" || req.NetworkType == "3G" || req.NetworkType == "2G" {
-		//	num = 2
-		//	res.MaxTrafficUse = GlobalConfig.MaxTrafficUse4g
-		//} else if req.NetworkType == "WIFI" {
-		//	num = 6
-		//	res.MaxTrafficUse = GlobalConfig.MaxTrafficUseWifi
-		//} else if req.NetworkType == "5G" {
-		//	num = 6
-		//	res.MaxTrafficUse = GlobalConfig.MaxTrafficUse5g
-		//} else {
-		//	num = 6
-		//	res.MaxTrafficUse = GlobalConfig.MaxTrafficUseOthers
-		//}
-		//res.ServerNum = min(num, len(req.ServersSortedByRTT))
-		////res.IpList = servers[:res.ServerNum]
-		////servers = append(servers[res.ServerNum:], servers[:res.ServerNum]...)
-		////fmt.Println(req.ServersSortedByRTT)
-		////fmt.Println(lastUse)
-		//lastUse.mux.Lock()
-		//defer lastUse.mux.Unlock()
-		//var doNotUse []string
-		//tp := 0
-		//for _, ip := range req.ServersSortedByRTT {
-		//	//fmt.Println(ip, time.Since(lastUse.lastUseTime[ip]).Milliseconds(), int64(GlobalConfig.TestTimeout))
-		//	if tp < res.ServerNum && time.Since(lastUse.lastUseTime[ip]).Milliseconds() >= int64(GlobalConfig.TestTimeout) {
-		//		tp++
-		//		lastUse.lastUseTime[ip] = time.Now()
-		//		res.IpList = append(res.IpList, ip)
-		//	} else {
-		//		doNotUse = append(doNotUse, ip)
-		//	}
-		//}
-		//for _, ip := range doNotUse {
-		//	if tp < res.ServerNum {
-		//		tp++
-		//		lastUse.lastUseTime[ip] = time.Now()
-		//		res.IpList = append(res.IpList, ip)
-		//	}
-		//}
-		//fmt.Println(res.IpList)
+		res.ServerNum, res.IpList = SS(eBandwidth)
 		c.JSON(http.StatusOK, res)
 	})
 	if err := r.Run(); err != nil {
